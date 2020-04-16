c3e300e6b1c2beef6bd971cc64bed35823ccd135
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-04-16 18:40:21 +0200

    Pravljenje Usera pomocu streama reko forEach

--- src/ba/unsa/etf/rs/tutorijal6/Main.java
+++ src/ba/unsa/etf/rs/tutorijal6/Main.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rs.tutorijal6;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -10,15 +11,18 @@ public class Main {
 	// write your code here
         User user = new FacebookUser("abcdef", "etf222", "Shadow OF ETF", LocalDate.now());
         ArrayList<FacebookUser> facebookUsers = new ArrayList<>();
-        new ArrayList<Integer>(
-                Arrays.asList(1,2,3,4,5)
+
+        facebookUsers.stream().forEach(facebookUser -> System.out.println("User: "+facebookUser));
+        facebookUsers = (ArrayList<FacebookUser>) new ArrayList<Integer>(
+                Arrays.asList(1, 2, 3, 4, 5)
         )
                 .stream()
-                .forEach(
-                        integer -> {
-                            facebookUsers.add(new FacebookUser(user.getUsername(), user.getPassword(), "AAA" + integer, LocalDate.of(2020, integer, integer)));
-                        }
-                );
-        facebookUsers.stream().forEach(facebookUser -> System.out.println("User: "+facebookUser));
+                .map(integer -> {
+                    return new FacebookUser(user.getUsername(), user.getPassword(), "AAA" + integer, LocalDate.of(2020, integer, integer));
+                }).collect(Collectors.toList());
+        System.out.println("---------------------------");
+        System.out.println(facebookUsers);
+
+
     }
 }
