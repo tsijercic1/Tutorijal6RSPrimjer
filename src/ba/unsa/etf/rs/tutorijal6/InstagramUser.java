bcb5366daa6947513627d94cf388b3bb8fce4cd8
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-04-16 18:27:39 +0200

    Prelazimo na streamove

---
+++ src/ba/unsa/etf/rs/tutorijal6/InstagramUser.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rs.tutorijal6;
+
+public class InstagramUser extends User {
+    private String page;
+    private Integer numberOfFollowers;
+
+
+    public InstagramUser(String username, String password) {
+        super(username, password);
+    }
+
+    public InstagramUser(String username, String password, String page, Integer numberOfFollowers) {
+        super(username, password);
+        this.page = page;
+        this.numberOfFollowers = numberOfFollowers;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public Integer getNumberOfFollowers() {
+        return numberOfFollowers;
+    }
+
+    public void setNumberOfFollowers(Integer numberOfFollowers) {
+        this.numberOfFollowers = numberOfFollowers;
+    }
+}
--- src/ba/unsa/etf/rs/tutorijal6/Main.java
+++ src/ba/unsa/etf/rs/tutorijal6/Main.java
@@ -1,8 +1,18 @@
 package ba.unsa.etf.rs.tutorijal6;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        User user = new FacebookUser("abcdef", "etf222", "Shadow OF ETF", LocalDate.now());
+        System.out.println(user);
+        FacebookUser facebookUser = (FacebookUser) user;
+        facebookUser.getUsername();
+        ArrayList<Integer> integers = new ArrayList<>();
+
+
     }
 }
